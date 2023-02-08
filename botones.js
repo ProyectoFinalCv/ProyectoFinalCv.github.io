6ddd43e0a36edd7817f5a3b7cc3c62562ec9a9a4
ProyectoFinalCv <124669519+ProyectoFinalCv@users.noreply.github.com>
2023-02-07 22:24:29 -0300

    Add files via upload

---
+++ botones.js
@@ -0,0 +1,14 @@
+
+document.getElementById('menu-cerrar').onclick = function () {
+    document.getElementById('menu-adaptable').style.width = "0%";
+}
+
+document.getElementById('menu-bars').onclick = function () {
+    document.getElementById('menu-adaptable').style.width = "100%";
+}
+
+
+document.getElementById('boton').onclick = function () {
+    document.getElementById('demo').innerHTML = `¿Como iniciar mis enversiones en bolsa? 2023`
+
+}
